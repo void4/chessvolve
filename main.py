d4c8be535a669ea46b1a484bb8158932a471b22c
void4 <arndtschnabel@freenet.de>
2020-12-18 18:08:34 +0100

    Use pool

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from time import time
 from random import random, choice
 from collections import Counter
 from trueskill import Rating, rate_1vs1
-from vm import execute, generate_random, mutate, F_GAS, splice, F_MEM, STARTGAS, F_IP
+from vm import execute, generate_random, mutate, F_GAS, splice, F_MEM, STARTGAS, F_IP, F_CODE, code_to_state
+import hashlib
 
 import chess
 
@@ -14,13 +15,19 @@ def num(board):
 
 index = 0
 
-
+MAXPOOL = 30
 
 pool = {}
 best = None
 queue = []
 
-def get_player():
+def detuple(tt):
+    return [list(t) for t in tt]
+
+def entuple(ll):
+    return tuple(tuple(l) for l in ll)
+
+def get_player2():
     global queue
     if random() < len(queue)/30 and len(queue) > 0:
         state = queue.pop(0)
@@ -28,13 +35,26 @@ def get_player():
         state = generate_random()
     return state
 
+def get_player():
+    if random() < 0.5 and len(pool) > 0:
+        return code_to_state(choice(list(pool.keys())))
+    else:
+        if random() < 0.5 and len(pool) > 2:
+            return code_to_state(mutate(splice(detuple(choice(list(pool.keys()))), detuple(choice(list(pool.keys()))))))
+        else:
+            return generate_random()
+
+
 while True:
-    print("queue", len(queue))
 
     p1 = get_player()
     p2 = get_player()
 
     board = chess.Board()
+
+
+
+    #out = []
 
     while not board.is_game_over():
         #print(board)
@@ -46,9 +66,12 @@ while True:
 
         has_output = False
 
+
         def output(value):
-            global has_output
+            #XXX use uci coordinates instead of index into legal?
+            global has_output, pool
             #print("OUTPUT", value)
+            #out.append(value)
             board.push(chess.Move.from_uci(legal[value%len(legal)]))
             has_output = True
 
@@ -59,6 +82,8 @@ while True:
         if not has_output:
             #print("NO OUTPUT")
             break
+
+    #print(out)
 
     draw = False
     if not has_output:
@@ -72,6 +97,8 @@ while True:
         res = board.result()
         if res == "1/2-1/2":
             draw = True
+            winner = p1
+            loser = p2
         else:
             if res == "1-0":
                 winner = p2
@@ -83,6 +110,7 @@ while True:
     if draw:
         pass
     else:
+        """
         winner[F_IP] = 0
         winner[F_GAS] = STARTGAS
         #if random() > len(queue)/30:
@@ -92,27 +120,40 @@ while True:
         if has_output:
             if random() > len(queue)/30:
                 queue.append(mutate(loser))
+        """
 
     gasdelta = winner[F_GAS] - STARTGAS
-    """
-    score = diffcolors# - gasdelta
 
-    if best is None or score > pool[best]:
-        print("New best: ", score, "Queue length: ", len(queue))
-        key = str(state)
-        pool[key] = score
-        best = key
+    winnercode = entuple(winner[F_CODE])
+    losercode = entuple(loser[F_CODE])
 
-        for i in range(50):
-            if random() > len(queue)/50:
-                if len(queue) > 0:
-                    other = choice(queue)
-                else:
-                    other = generate_random()
+    #win, draw, loss
+    winnerdata = pool.get(winnercode, [Rating(), 0, 0, 0, []])
+    loserdata = pool.get(losercode, [Rating(), 0, 0, 0, []])
 
-                newstate = mutate(splice(other, state))
-                newstate[F_X] = state[F_X]
-                newstate[F_Y] = state[F_Y]
-                newstate[F_R] = state[F_R]
-                queue.append(newstate)
-    """
+    if draw:
+        winnerdata[2] += 1
+        loserdata[2] += 1
+    else:
+        winnerdata[1] += 1
+        loserdata[3] += 1
+
+
+    winnerrating, loserrating = rate_1vs1(winnerdata[0], loserdata[0], drawn=draw)
+    winnerdata[0] = winnerrating
+    loserdata[0] = loserrating
+    pool[winnercode] = winnerdata
+    pool[losercode] = loserdata
+
+    if not has_output:
+        del pool[losercode]
+
+
+    os.system("clear")
+    toplist = sorted(pool.items(), key=lambda kv:kv[1][0].mu, reverse=True)
+    for k,v in toplist[MAXPOOL:]:
+        del pool[k]
+
+    toplist = toplist[:MAXPOOL]
+    for k,v in toplist:
+        print(hashlib.sha256(str(k).encode("ascii")).hexdigest()[:6], v)#, k[:10])
---
+++ vm.py
@@ -0,0 +1,144 @@
+from random import randint, choice
+from copy import deepcopy
+
+# Instruction numbers
+NUMINSTR = 8
+I_PUSH, I_ADD, I_PRINT, I_JUMP, I_SUB, I_IP, I_RANDOM, I_MEMREAD = range(NUMINSTR)#, I_MEMWRITE
+
+CELL_SIZE = 256
+CELL_MAX = CELL_SIZE-1
+CELL_MIN = 0
+
+# indexes of the state components
+NUMFIELDS = 4
+F_GAS, F_IP, F_CODE, F_MEM = range(NUMFIELDS)
+
+STARTGAS = 512
+CODEGEN = 256
+
+def execute(output, state):
+
+	# Runtime stack for data manipulation
+	stack = []
+
+	start_steps = state[F_GAS]
+	OUTPUT = False
+
+	while True:
+
+		if OUTPUT:
+			break
+
+		# Assuming no refills
+		steps_done = start_steps - state[F_GAS]
+
+		# pseudo-gravity
+		"""
+		if steps_done % 20 == 0:
+			if state[F_Y] < H-1:
+				state[F_Y] = (state[F_Y]+1)%H
+		"""
+
+		# Uncomment this to see the program state and stack each step
+		#print("STATE:", state)
+		#print("STACK:", stack)
+
+		steps = state[F_GAS]
+		ip = state[F_IP]
+		code = state[F_CODE]
+
+		# We are out of steps, stop the process
+		if steps <= 0:
+			break
+
+		# No instructions are left, stop the process
+		if ip >= len(code) or ip < 0:
+			break
+
+		if len(code[ip]) == 2:
+			# Some instructions have an argument, unpack that as well
+			instruction, argument = code[ip]
+		else:
+			instruction = code[ip][0]
+			argument = None
+
+
+		if instruction == I_PUSH:
+			# Push the argument to the top of the stack
+			stack.append(argument)
+		elif instruction == I_ADD:
+			# Pop the two topmost elements from the stack, add them and push the result back on the stack
+			if len(stack) >= 2:
+				stack.append(stack.pop(-1) + stack.pop(-1))
+		elif instruction == I_SUB:
+			# Pop the two topmost elements from the stack, subtract them and push the result back on the stack
+			if len(stack) >= 2:
+				stack.append(stack.pop(-1) - stack.pop(-1))
+		elif instruction == I_MEMREAD:
+			if len(stack) >= 1:
+				stack.append(state[F_MEM][stack.pop(-1)%len(state[F_MEM])])
+		#elif instruction == I_MEMWRITE:
+		#	if len(stack) >= 2:
+		#		state[F_MEM][stack.pop(-1)%len(state[F_MEM])] = stack.pop(-1)
+		#TODO allow allocating memory? fixed size? read only area for input state?
+		elif instruction == I_PRINT:
+			# Pop the topmost element from the stack and print it
+			#print("OUTPUT:", stack.pop(-1))
+			if len(stack) >= 1:
+				output(stack.pop(-1))
+				OUTPUT = True
+		elif instruction == I_JUMP:
+			if len(stack) >= 1:
+				state[F_IP] = stack.pop(-1)
+		elif instruction == I_IP:
+			stack.append(ip)
+		elif instruction == I_RANDOM:
+			stack.append(randint(0,255))
+		# Move the instruction pointer one step forward to point to the next instruction
+		state[F_IP] += 1
+
+		# Reduce the number of remaining steps by one
+		state[F_GAS] -= 1
+
+		# Print a newline after each iteration
+		#print("")
+
+def code_to_state(code):
+	return [STARTGAS, 0, code, []]
+
+def random_instruction():
+	instr = randint(0, NUMINSTR-1)
+	if instr == I_PUSH:
+		block = [I_PUSH, randint(0,CELL_MAX)]
+	else:
+		block = [instr]
+	return block
+
+def generate_random():
+	code = []
+	for i in range(CODEGEN):
+		code.append(random_instruction())
+
+	return code_to_state(code)
+
+def mutate(code):
+	for i in range(len(code)//10):
+		random_index = randint(0, len(code)-1)
+		code[random_index] = random_instruction()
+	# Replenish, reset
+	return code
+
+def splice(code1, code2):
+	code1 = deepcopy(code1)
+	code2 = deepcopy(code2)
+	index = randint(0, min(len(code1), len(code2))-1)
+	first, second = (code1, code2) if randint(0,1) == 0 else (code2, code1)
+	code = first[:index] + second[index:]
+	return code
+
+if __name__ == "__main__":
+	# full runtime state of a process
+	# number of remaining steps, instruction pointer, instruction list
+	state_example = [100, 0, 0, 0, 0, [[I_PUSH, 1], [I_PUSH, 2], [I_ADD], [I_PRINT], [I_JUMP]]]
+
+	execute(state_example)
